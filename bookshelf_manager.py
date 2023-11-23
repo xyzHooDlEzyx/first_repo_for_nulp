c13c48c7215b642414e5018362934bfc53e01ec6
xyzHooDlEzyx <93081557+xyzHooDlEzyx@users.noreply.github.com>
2023-11-23 10:24:02 +0200

    implemented suggesed changes bookshelf_manager.py
    renamed intances and deleted useless comment

--- bookshelf_manager.py
+++ bookshelf_manager.py
@@ -3,17 +3,17 @@ from bookshelf import BookShop
 
 
 def main():
-    book1 = Book("Song of Ice and Fire", "J. Martin", 400, 200, 1000, 250)
-    book2 = Book("Harry Potter", "Joanne Rowling", 400, 100, 8000, 4000)
-    book3 = Book("JoJo Bizarre Adventures", "H. Araki", 900, 300, 10000, 8000)
+    song_of_ice_and_fire = Book("Song of Ice and Fire", "J. Martin", 400, 200, 1000, 250)
+    harry_potter = Book("Harry Potter", "Joanne Rowling", 400, 100, 8000, 4000)
+    jojo_bizarre_adventures = Book("JoJo Bizarre Adventures", "H. Araki", 900, 300, 10000, 8000)
 
     bookshop = BookShop()
-    bookshop.add_book(book1)
-    bookshop.add_book(book2)
-    bookshop.add_book(book3)
+    bookshop.add_book(song_of_ice_and_fire)
+    bookshop.add_book(harry_potter)
+    bookshop.add_book(jojo_bizarre_adventures)
 
-    top_price_books = bookshop.top_books_by_price(2)
-    top_sales_books = bookshop.top_books_by_sales(2)
+    get_top_price_books = bookshop.get_top_books_by_price(2)
+    get_top_sales_books = bookshop.get_top_books_by_sales(2)
 
     print("\nTop books by price:")
     for book in top_price_books:
@@ -23,9 +23,6 @@ def main():
         print(book)
 
     bookshop.del_book(book1)
-
-
-# bookshop.del_book(book1)
 
 
 if __name__ == '__main__':
